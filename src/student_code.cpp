1be37ff3c0b41147244d6bbcfd2d2f8610d4e950
shabansatti-cyber <shaban_satti@hotmail.co.uk>
2026-02-18 11:46:31 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -15,7 +15,7 @@ float getFloatValue() {
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to a.
-    char var_char = 'a';
+    char var_char = 'A';
     return var_char;
 }
 
